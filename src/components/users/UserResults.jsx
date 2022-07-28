7b452d1d0808b0caedd954c529557fea78c5dda7
Welschoff <h.welschoff@gmx.de>
2022-07-28 13:44:28 +0200

    Finished udemy tutorial

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useContext } from 'react';
+import Spinner from '../layouts/Spinner';
+import UserItem from './UserItem';
+import GitHubContext from '../../../src/context/github/GithubContext';
+
+function UserResults() {
+  const { loading, users } = useContext(GitHubContext);
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,29 @@
+import { createContext } from 'react';
+import { useReducer } from 'react';
+import githubReducer from './GithubReducer';
+
+const GitHubContext = createContext();
+
+export const GitHubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  return (
+    <GitHubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GitHubContext.Provider>
+  );
+};
+
+export default GitHubContext;
